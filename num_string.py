581568765e7bef791ce4852a22960c214e8c368d
Justin Schechter <justin@nextdoorco.com>
2015-12-19 10:52:26 -0500

    completed test harness all tests pass

--- num_string.py
+++ num_string.py
@@ -9,6 +9,15 @@ names = dict([(0, 'ZERO'),
               (8, 'EIGHT'),
               (9, 'NINE'),
               (10, 'TEN'),
+              (11, 'ELEVEN'),
+              (12, 'TWELVE'),
+              (13, 'THIRTEEN'),
+              (14, 'FOURTEEN'),
+              (15, 'FIFTEEN'),
+              (16, 'SIXTEEN'),
+              (17, 'SEVENTEEN'),
+              (18, 'EIGHTEEN'),
+              (19, 'NINETEEN'),
               (20, 'TWENTY'),
               (30, 'THIRTY'),
               (40, 'FORTY'),
@@ -21,6 +30,8 @@ names = dict([(0, 'ZERO'),
               (1000, 'THOUSAND')])
 
 def text_from_num(cents):
+    if (cents <= 0) or (cents > 99999999):
+        raise(ValueError('invalid input'))
     num = int(cents)
     sb = []
     val = num - ((num // 100) * 100)
@@ -46,9 +57,32 @@ def text_from_num(cents):
     num = num // 100
     val = num - ((num // 10) * 10)
     if (val > 0):
+        if sb[len(sb)-1] == names[0]:
+            sb.pop()
         sb.append(names[100])
         sb.append(names[val])
         
+    num = num // 10
+    if (num > 0):
+        if sb[len(sb)-1] == names[0]:
+            sb.pop()        
+        sb.append(names[1000])
+        
+        val = num - ((num // 100) * 100)
+        if val in names:
+            sb.append(names[val])
+        else:
+            tens = (val // 10) * 10
+            ones = val - tens
+            sb.append(names[ones])
+            sb.append(names[tens])        
+        
+        num = num // 100
+        if (num > 0):
+            if sb[len(sb)-1] == names[0]:
+                sb.pop()
+            sb.append(names[100])    
+            sb.append(names[num])    
             
     return ' '.join(sb[::-1])
 
--- test_num_string.py
+++ test_num_string.py
@@ -6,13 +6,37 @@ import num_string
     (12, 'ZERO AND TWELVE'),
     (65, 'ZERO AND SIXTY FIVE'),
     (400, 'FOUR AND ZERO'),
+    (313, 'THREE AND THIRTEEN'),
     (543, 'FIVE AND FORTY THREE'),
     (2424, 'TWENTY FOUR AND TWENTY FOUR'),
+    (1918, 'NINETEEN AND EIGHTEEN'),
     (7823, 'SEVENTY EIGHT AND TWENTY THREE'),
+    (6000, 'SIXTY AND ZERO'),
+    (70000, 'SEVEN HUNDRED AND ZERO'),
+    (13142, 'ONE HUNDRED THIRTY ONE AND FORTY TWO'),
     (73219, 'SEVEN HUNDRED THIRTY TWO AND NINETEEN'),
     (32424, 'THREE HUNDRED TWENTY FOUR AND TWENTY FOUR'),
+    (71516, 'SEVEN HUNDRED FIFTEEN AND SIXTEEN'),
+    (100000, 'ONE THOUSAND AND ZERO'),
+    (601213, 'SIX THOUSAND TWELVE AND THIRTEEN'),
+    (478293, 'FOUR THOUSAND SEVEN HUNDRED EIGHTY TWO AND NINETY THREE'),
+    (5000000, 'FIFTY THOUSAND AND ZERO'),
+    (3131118, 'THIRTY ONE THOUSAND THREE HUNDRED ELEVEN AND EIGHTEEN'),
+    (9965467, 'NINETY NINE THOUSAND SIX HUNDRED FIFTY FOUR AND SIXTY SEVEN'),
+    (70000000, 'SEVEN HUNDRED THOUSAND AND ZERO'),
+    (71421518, 'SEVEN HUNDRED FOURTEEN THOUSAND TWO HUNDRED FIFTEEN AND EIGHTEEN'),
+    (83248293, 'EIGHT HUNDRED THIRTY TWO THOUSAND FOUR HUNDRED EIGHTY TWO AND NINETY THREE')
 ])
 
-def test_test(input, expected):
+def test_standard(input, expected):
     result = num_string.text_from_num(input)
+    print(result)
     assert(result == expected)
+    
+def test_lower_bound():
+    with pytest.raises(ValueError):
+        num_string.text_from_num(0)
+        
+def test_upper_bound():
+    with pytest.raises(ValueError):
+        num_string.text_from_num(100000000)    
