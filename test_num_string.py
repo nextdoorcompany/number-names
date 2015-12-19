7f9e16e58878ed1bcc85bd4a05440e422d1bc19c
Justin Schechter <justin@nextdoorco.com>
2015-12-18 22:29:16 -0500

    added more tests

--- test_num_string.py
+++ test_num_string.py
@@ -1,6 +1,18 @@
 import pytest
 import num_string
 
-def test_test():
-    result = num_string.text_from_num(400)
-    assert(result == 'FOUR AND ZERO')
+@pytest.mark.parametrize('input,expected', [    
+    (1, 'ZERO AND ONE'),
+    (12, 'ZERO AND TWELVE'),
+    (65, 'ZERO AND SIXTY FIVE'),
+    (400, 'FOUR AND ZERO'),
+    (543, 'FIVE AND FORTY THREE'),
+    (2424, 'TWENTY FOUR AND TWENTY FOUR'),
+    (7823, 'SEVENTY EIGHT AND TWENTY THREE'),
+    (73219, 'SEVEN HUNDRED THIRTY TWO AND NINETEEN'),
+    (32424, 'THREE HUNDRED TWENTY FOUR AND TWENTY FOUR'),
+])
+
+def test_test(input, expected):
+    result = num_string.text_from_num(input)
+    assert(result == expected)
